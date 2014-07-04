4e83ae9139a3b874376837aff6e179e53499fbe5
turbochip <ccox@Chips-MacBook-Pro.local>
2014-07-04 16:42:52 -0500

    trying to re-write contacts control

---
+++ PunchList/PLContactsTVC.h
@@ -0,0 +1,17 @@
+//
+//  PLContactsTVC.h
+//  PunchList
+//
+//  Created by Chip Cox on 7/4/14.
+//  Copyright (c) 2014 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AddressBook/AddressBook.h>
+#import <AddressBookUI/AddressBookUI.h>
+
+@interface PLContactsTVC : UITableViewController
+@property (strong, nonatomic) NSMutableArray *contacts;
+
+
+@end
---
+++ PunchList/PLContactsTVC.m
@@ -0,0 +1,145 @@
+//
+//  PLContactsTVC.m
+//  PunchList
+//
+//  Created by Chip Cox on 7/4/14.
+//  Copyright (c) 2014 Home. All rights reserved.
+//
+
+#import "PLContactsTVC.h"
+
+@interface PLContactsTVC ()
+@end
+
+@implementation PLContactsTVC
+
+- (NSMutableArray *) contacts
+{
+    if(!_contacts) _contacts=[[NSMutableArray alloc] init];
+    return _contacts;
+}
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    ABAddressBookRef aBook=ABAddressBookCreateWithOptions(NULL, NULL);
+    PLContactsTVC * __weak weakSelf = self;  // avoid capturing self in the block
+    ABAddressBookRequestAccessWithCompletion(aBook,
+                                             ^(bool granted, CFErrorRef error) {
+                                                 if (granted) {
+                                                     NSArray *contacts = CFBridgingRelease(
+                                                                                            ABAddressBookCopyPeopleWithName(aBook,
+                                                                                                                            CFSTR("Cox")
+                                                                                                                            )
+                                                                                            );
+                                                 } else {
+                                                     // Handle the case of being denied access and/or the error.
+                                                 }
+                                                 CFRelease(aBook);
+                                             });
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 26;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+- (IBAction)searchContacts:(UIButton *)sender {
+    
+    ABPeoplePickerNavigationController *picker =[[ABPeoplePickerNavigationController alloc] init];
+    picker.peoplePickerDelegate = self;
+    
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+- (void) newPersonViewController:(ABNewPersonViewController *)newPersonView didCompleteWithNewPerson:(ABRecordRef)person
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)peoplePickerNavigationControllerDidCancel:
+(ABPeoplePickerNavigationController *)peoplePicker
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
+
+- (BOOL)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker
+      shouldContinueAfterSelectingPerson:(ABRecordRef)person {
+    
+    [self displayPerson:person];
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    return NO;
+}
+
+- (BOOL)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker
+      shouldContinueAfterSelectingPerson:(ABRecordRef)person
+                                property:(ABPropertyID)property
+                              identifier:(ABMultiValueIdentifier)identifier
+{
+    return NO;
+}
+
+- (void)displayPerson:(ABRecordRef)person
+{
+}
+
+
+@end
